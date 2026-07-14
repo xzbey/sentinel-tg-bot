8bce36cb97c7ff157fb6cd85fd413a5e3f4a0213
xzbey <O_O_X_X_2006@mail.ru>
2026-07-14 21:01:47 +0300

    transition to copernicus

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ def sentinel_config():
     config = SHConfig()
     config.sh_client_id = os.getenv("SH_CLIENT_ID")
     config.sh_client_secret = os.getenv("SH_CLIENT_SECRET")
+    config.sh_base_url = "https://sh.dataspace.copernicus.eu"
+    config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
     config.sh_timeout = 60
     return config
 
